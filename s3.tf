ee4a2e8fb81fc10dcbe0189fa37c6a8f8d2a0127
Gangabhavani-tentu <119149667+Gangabhavani-tentu@users.noreply.github.com>
2024-11-13 16:02:51 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,4 +1,8 @@
 resource "aws_s3_bucket" "my_bucket" {
   bucket = var.s3_bucket_name
   # Remove acl from here
+website {
+    index_document = "index.html"
+    # other configurations if needed
+  }
 }
