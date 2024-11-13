3f24e0e6854314f5e020b7a09a49caabea640c20
Gangabhavani-tentu <119149667+Gangabhavani-tentu@users.noreply.github.com>
2024-11-13 14:46:40 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -32,7 +32,7 @@ variable "availability_zone" {
 
 variable "s3_bucket_name" {
   description = "S3 Bucket Name"
-  default     = "my-bucket-unique-name"
+  default     = "my-unique-bucket-name-123456"
 }
 
 variable "region" {
