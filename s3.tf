bc427ce118be704991fd4190daece67b3491ec25
Peeta Bala Yakshitha <139906460+yakshi26@users.noreply.github.com>
2024-11-13 16:12:34 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,4 +1,10 @@
 resource "aws_s3_bucket" "my_bucket" {
   bucket = var.s3_bucket_name
-  # Remove acl from here
+ 
+}
+
+resource "aws_s3_object" "my_file" {
+  bucket = aws_s3_bucket.my_bucket.bucket
+  key    = "1311file.html"
+  source = "C:/Users/91996/Desktop/new html/digital watch/dg.html"  
 }
