8e28cc02c27ce3daf325fa23bbc56bf2c27a05e4
Gangabhavani-tentu <119149667+Gangabhavani-tentu@users.noreply.github.com>
2024-11-13 16:11:09 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,15 +1,16 @@
 resource "aws_s3_bucket" "my_bucket" {
   bucket = var.s3_bucket_name
-  # Remove acl from here
-website {
+
+  website {
     index_document = "index.html"
-    # other configurations if needed
+    # Add other configurations if needed
   }
 }
 
 resource "aws_s3_bucket_object" "index_html" {
-  bucket = aws_s3_bucket.static_website.bucket
+  bucket = aws_s3_bucket.my_bucket.bucket  # Reference the correct bucket
   key    = "index.html"
-  source = "D:\html\portfolio\personal-portfolio-img\index.html"  # Local file to upload to S3
-  acl    = "public-read"
+  source = "D:/html/portfolio/personal-portfolio-img/index.html"  # Ensure the correct file path for your local machine
+  acl    = "public-read"  # Optional: If you want it publicly accessible
 }
+
