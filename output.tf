e51b1b053cbe080805300f154a522cffda9728c4
Gangabhavani-tentu <119149667+Gangabhavani-tentu@users.noreply.github.com>
2024-11-24 20:18:47 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -21,5 +21,5 @@ output "s3_bucket_name" {
 }
 
 output "static_website_url" {
-  value = aws_s3_bucket.my_bucket.website_endpoint
+  value = aws_s3_bucket_website_configuration.my_bucket_website.website_endpoint
 }
